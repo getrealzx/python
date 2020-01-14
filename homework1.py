dee59df4bcdbf4d7db01be8070c781504d655165
Richard Zhang <getrealzx@gmail.com>
2020-01-14 15:59:07 -0600

    guess number v1

--- homework1.py
+++ homework1.py
@@ -9,14 +9,32 @@
 
 #Factor Numbers
 
-asknum = input("please input a number >>")
-x = int(asknum)
-i = 1
-print("the factor of", x, "are")
-while i < x:
-    if x % i ==0:
-        print(i)
-    i +=1
+# asknum = input("please input a number >>")
+# x = int(asknum)
+# i = 1
+# print("the factor(s) of", x, "are")
+# while i < x:
+#     if x % i ==0:
+#         print(i)
+#     i +=1
     
+#Guess a Number
+# #Guess a Number V1
+# print("I am thinking of a number bewteen 1 and 10")
+
+# gn = 0
+# while gn != 5:
+#     gn = int(input("What's the number?"))
+
+# print("Yes! You win!")
 
 
+
+#Guess a Number V2
+print("I am thinking of a number bewteen 1 and 10")
+
+gn = 0
+while gn != 5:
+    gn = int(input("What's the number?"))
+
+print("Yes! You win!")
