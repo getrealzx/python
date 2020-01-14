80d5fd37d36c7b6cff363815e24074e7c683b6aa
Richard Zhang <getrealzx@gmail.com>
2020-01-14 15:24:53 -0600

    hw1-Triangle Number

---
+++ homework1.py
@@ -0,0 +1,11 @@
+#first 100 triangle number
+count = 0
+tn = 1 #tn is triangle number
+while count < 100:
+    count +=1
+    tn = count*((count+1)/2)
+    print(tn)
+print("those are first 100 triangle numbers")
+
+
+
